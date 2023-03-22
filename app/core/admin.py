14341b8201361ceb06531833197c2d5f3485176c
Uvais-1 <4so19cs092.uvais@sjec.ac.in>
2023-03-22 12:47:09 +0530

    configured admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin #noqa
+"""
+Django Admin customisation
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """Define Admin pages for users."""
+    odrering = ['id']
+    list_display = ['email', 'name']
+
+admin.site.register(models.user, UserAdmin)
